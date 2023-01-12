38dbfcacf819604eb89114135b49ea6a7b015c7d
Ali Berke Geçgel <47008493+aliberke1910@users.noreply.github.com>
2023-01-12 23:52:29 +0300

    Add files via upload

---
+++ Week-2/inheritance/src/inheritance/CustomerManager.java
@@ -0,0 +1,11 @@
+package inheritance;
+
+public class CustomerManager {
+	public void List () {
+		System.out.println("Listelendi!");
+	}
+	
+	public void Add () {
+		System.out.println("Eklendi!");
+	}
+}
